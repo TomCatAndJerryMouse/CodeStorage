9a527e4c1e8f55b07b36efa55c00e868755a498e
tengyi <18628387154@sina.com>
2022-08-09 23:51:01 +0800

    好久之前的。。。忘记了

---
+++ src/com/ty/utils/memcached/MemcachedManger.java
@@ -0,0 +1,18 @@
+package com.ty.utils.memcached;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import net.spy.memcached.MemcachedClient;
+
+public class MemcachedManger {
+	public static void main(String[] args) {
+		try {
+			MemcachedClient mc = new  MemcachedClient(new InetSocketAddress("139.9.181.229",11211));
+			 System.out.println("Connection to server sucessful.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
