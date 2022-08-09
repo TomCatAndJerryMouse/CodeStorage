6e6412793d718a639ad2d6299bc08a32a1a6f9ca
tengyi <18628387154@sina.com>
2022-08-10 02:23:38 +0800

    jks获取证书签名验签

--- java/src/main/java/cn/ty/util/certificate/Index.java
+++ java/src/main/java/cn/ty/util/certificate/Index.java
@@ -3,30 +3,29 @@ package cn.ty.util.certificate;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.Signature;
-import java.security.UnrecoverableKeyException;
+import java.net.URLDecoder;
+import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.Base64;
-
-import com.sun.xml.internal.ws.util.StringUtils;
 
 public class Index {
 	// 密钥库密码
 	private static String storepass = "123123";
 	// 证书库路径
-	private static String storePath = Index.class.getResource("").toString().replace("file:/", "") + "mytest.keystore";
+	//private static String storePath = Index.class.getResource("").toString().replace("file:/", "") + "mytest.keystore";
+	private static String storePath = "";
 	// 证书别名
 	private static String certAlias  = "mytest";
 	// 密钥库类型
 	private static String keyType = "JKS";
-	
-	public static void main(String[] args) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException, InvalidKeyException {
+
+	public static void main(String[] args) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException, InvalidKeyException, SignatureException {
+		storePath = URLDecoder.decode(Index.class.getClassLoader().getResource("mytest.keystore").getPath().toString(),"UTF-8");
+		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+			storePath = storePath.substring(1);
+		}
+		System.out.println(storePath);
 		// 
 		FileInputStream fi = new FileInputStream(storePath);
 		// 初始化密钥库 
@@ -34,7 +33,7 @@ public class Index {
 		ks.load(fi,storepass.toCharArray());
 		// 获取证书
 		Certificate cf = ks.getCertificate(certAlias);
-		
+
 		System.out.println(Base64.getEncoder().encodeToString(cf.getEncoded()));
 //		ks.getCertificate(alias);
 		
@@ -43,7 +42,15 @@ public class Index {
 		System.out.println(Base64.getEncoder().encodeToString(priKey.getEncoded()));
 		
 		// 用私钥签名
-		Signature sig = Signature.getInstance("SHA1withDSA");
+		Signature sig = Signature.getInstance("SHA256withRSA");
 		sig.initSign(priKey);
+		sig.update("123123".getBytes());
+		String ss = Base64.getEncoder().encodeToString(sig.sign());
+		System.out.println("签名值："+ss);
+
+
+		sig.initVerify(cf.getPublicKey());
+		sig.update("123123".getBytes());
+		System.out.println("验证签名结果："+sig.verify(Base64.getDecoder().decode(ss)));
 	}
 }
